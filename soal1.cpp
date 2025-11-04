86c10e67e9e73a4f7cc0abfa891744dee8dda279
24003Rian <sitisopiahsiregar322@gmail.com>
2025-11-04 20:39:07 +0700

    Update soal1.cpp

--- soal1.cpp
+++ soal1.cpp
@@ -1,1 +1,26 @@
-//Masukkan jawaban disini
+#include <iostream>
+using namespace std;
+
+// Fungsi untuk melakukan rotasi kiri
+void rotasiKiri(int arr[], int n, int d) {
+    d = d % n; //ini untuk menghindari rotasinya supaya tidak berlebihan yang hasilnya sama
+    for (int i = 0; i < n; i++) {
+        cout << arr[(i + d) % n] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n, d;
+    cout << "Masukkan n dan banyak rotasi: ";
+    cin >> n >> d;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    rotasiKiri(arr, n, d);
+
+    return 0;
+}
